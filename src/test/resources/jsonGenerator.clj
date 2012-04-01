66564447f22eed8d4e301046a3812eb21f534f59
Anders Karlsen <karlsenanders@hotmail.com>
2012-04-01 17:52:01 +0200

    Added split pair function

--- src/test/resources/jsonGenerator.clj
+++ src/test/resources/jsonGenerator.clj
@@ -13,5 +13,23 @@
     (str (quoted key) " : " (quoted value)))
   (is (= "\"firstName\" : \"Darth\"" (key-value "firstName" "Darth"))))
 
+(with-test 
+  (defn split-into-pairs [x]
+    ((fn [v result]
+    (if (empty? v) result
+      (recur (-> v rest rest) (into result [[(first v) (second v)]]))
+      )) x [])
+    )
+    (is (= [[1 2] [3 4]] (split-into-pairs [1 2 3 4])))
+    (is (= [[1 2] [3 4] [5 6]] (split-into-pairs [1 2 3 4 5 6])))
+    )
+
+; Not ready for this yet...
+;(with-test
+;  (defn key-value-list [key-values]
+;  "3")
+;  (is (= "{ \"firstName\" : \"Darth\", \"lastName\" : \"Vader\" }" (key-value-list ["firstname" "Darth" "lastname" "Vader"])))
+;  )
+
 (run-tests)
 
